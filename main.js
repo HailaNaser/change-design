60f7b928396647c7662a40d41eabd3ca0d971586
HailaNasser <hailaalnasser05@gmail.com>
2024-05-28 15:59:29 +0300

    v1

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ let roverLeft = document.querySelector(".rover-header-left")
 let roverRight = document.querySelector(" .rover-header-right")
 let roverHero = document.querySelector(".rover-mobile-hero")
 let theSecondImage = document.querySelector(".bg-w img")
+let h2 = document.querySelector(".rover-123-container h2 ")
+
+
 
 logo.addEventListener("click" , (e)=> {
     main.classList.add("version-two");
@@ -24,8 +27,9 @@ logo.addEventListener("click" , (e)=> {
     roverQuote.insertAdjacentHTML("afterbegin" , `My Rover sitter sent me updates throughout the day and took care of my cat as if she were her own. <span>– Corinna F.</span>`);
     // learnMore.insertAdjacentHTML("afterbegin",`We’re The Cat People. <a href="#">Learn More</a>`)
     learnMore.innerHTML = `We’re The Cat People.<a href="#">Learn More</a>`
-    roverHero.src = "https://i.ibb.co/QfqjHxq/mobile-hero-2.jpg"
+    roverHero.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGPYZpO3CZfDNjwPGsc2iiWAIN8fvj2BinwxHx6z8PGQ&s"
     theSecondImage.src = "https://images.hdqwalls.com/download/cute-cat-4k-wg-1080x1920.jpg"
+    h2.textContent = "We’re the treat-your-Cat-like-family Cat people"
 })
 menu.addEventListener("click" , ((e)=> {
     roverLeft.style.display = "block";
